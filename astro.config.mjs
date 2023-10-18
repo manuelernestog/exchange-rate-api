bdf1958cba8ef5dfb3d0fa404c9a89d07640c7cb
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2023-10-18 13:48:39 -0400

    removing github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://exchange-rate.decubba.com',
-  });
+export default defineConfig({});
